04c6f05615dc02cef3010de2c52d74f4a5050d65
josef anolin <josefanolin@josefs-MacBook-Pro-2.local>
2021-02-04 21:42:59 -0800

    Module Complete

---
+++ plots.js
@@ -0,0 +1,41 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+function optionChanged(newSample) {
+  console.log(newSample);
+}
+
+// print information to the Demographic Infopanel: once a user selects an ID number, the associated volunteer's demographic information needs to be filtered from 
+//samples.json and placed in the panel. 
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      Object.entries(result).forEach(([key,value])=>{
+          PANEL.append("h6").text(`${key}:${value}`)
+        });
+      
+    });
+  }
